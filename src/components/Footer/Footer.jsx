3cb3b5229b542b53f0fbe54efa406e49606d407a
Phil Hancox <phil.hancox@revolutionbarsgroup.com>
2025-01-24 15:24:43 +0000

    Footer styles init

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBluesky, faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {faPoo} from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
-  return <footer>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <p className="text-center">Copyright &copy; Phil Hancox 2025</p>
+    return <footer>
+        <div style={{padding: '20px 0'}}>
+            <a href="https://bsky.app/profile/phlhn.cx" target="_blank" rel="noreferrer noopener">
+                <FontAwesomeIcon size="lg" icon={faBluesky}/></a>
+            <a href="https://github.com/philhancox" target="_blank" rel="noreferrer noopener">
+                <FontAwesomeIcon size="lg" icon={faGithub}/></a>
+            <a href="hhttps://www.linkedin.com/in/philhancox/" target="_blank" rel="noreferrer noopener">
+                <FontAwesomeIcon size="lg" icon={faLinkedin}/></a>
         </div>
-      </div>
-    </div>
-  </footer>;
+        <p className="text-center">Copyright &copy; Phil Hancox {new Date().getFullYear()}</p>
+        <small>Last updated {new Date().toLocaleString('en-gb')}</small>
+    </footer>;
 }
 
 export default Footer;
