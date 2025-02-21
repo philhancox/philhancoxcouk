eb536193f90df3be87375592886f1587bc9d85dd
Phil Hancox <phil.hancox@revolutionbarsgroup.com>
2025-02-21 16:59:36 +0000

    Title change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
           <p className="lead">👋🏻 Hello! My name is
             <span className={styles.phil} itemProp="name">{' '}
               <span itemProp="givenName">Phil</span> <span itemProp="familyName">Hancox</span>
-            </span><br />and I'm a <span itemProp="jobTitle">web developer</span> based in
+            </span> and I'm a <span itemProp="jobTitle">web developer</span> based in
             {' '}<span itemProp="homeLocation">Sheffield</span>, <span itemProp="nationality">England</span></p>
       </div>
     </div>
